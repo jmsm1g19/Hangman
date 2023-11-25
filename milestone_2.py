515aa07963c6b3fda75b4b382ea6522c440e7db9
Jacob Softleigh-Moore <jacob.smoore@hotmail.co.uk>
2023-11-25 17:54:51 +0000

    first commit

---
+++ milestone_2.py
@@ -0,0 +1,11 @@
+
+# Milestone 2 for Hangman Game
+
+# Step 1: Create a list containing the names of 5 favorite fruits
+favorite_fruits = ['apple', 'banana', 'cherry', 'orange', 'grape']
+
+# Step 2: Assign this list to a variable called word_list
+word_list = favorite_fruits
+
+# Step 3: Print out the newly created list
+print(word_list)
---
+++ milestone_3.py
@@ -0,0 +1,10 @@
+
+# Milestone 3 for Hangman Game
+import random
+
+favorite_fruits = ['apple', 'banana', 'cherry', 'orange', 'grape']
+
+word_list = favorite_fruits
+
+word = random.choice(word_list)
+print(word)
