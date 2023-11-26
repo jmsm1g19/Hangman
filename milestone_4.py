078314459cd2fcb0f54d46bb6ce141a07cf592b4
Jacob Softleigh-Moore <jacob.smoore@hotmail.co.uk>
2023-11-26 11:57:41 +0000

    created milestone 4 class

--- milestone_4.py
+++ milestone_4.py
@@ -1,28 +1,38 @@
 import random
 
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.word_list = word_list
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        """
+        Check if the guessed letter is in the word.
+        """
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+    def ask_for_input(self):
+        """
+        Prompt the user for a letter and validate the input.
+        """
+        while True:
+            guess = input("Guess a letter: ")
+
+            if not (len(guess) == 1 and guess.isalpha()):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.list_of_guesses.append(guess)
+                self.check_guess(guess)
+                break
+
 favorite_fruits = ['apple', 'banana', 'cherry', 'plum', 'grape']
-word = random.choice(favorite_fruits)
-print('Word: ', word)
-def check_guess(guess):
-    """
-    Check if the guessed letter is in the word.
-    """
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
-
-def ask_for_input():
-    """
-    Prompt user for a letter and check if it's a valid guess.
-    """
-    while True:
-        guess = input("Guess a letter: ")
-        if len(guess) == 1 and guess.isalpha():
-            check_guess(guess)
-            break 
-        else:
-            print("Invalid letter. Please, enter a single alphabetical character.")
-
-ask_for_input()
+game = Hangman(favorite_fruits)
+game.ask_for_input()
