88ceae4dda53be35c2022782b9a766a7cc6d1ff8
Jacob Softleigh-Moore <jacob.smoore@hotmail.co.uk>
2023-11-26 01:09:59 +0000

    init milestone 4

---
+++ milestone_4.py
@@ -0,0 +1,28 @@
+import random
+
+favorite_fruits = ['apple', 'banana', 'cherry', 'plum', 'grape']
+word = random.choice(favorite_fruits)
+print('Word: ', word)
+def check_guess(guess):
+    """
+    Check if the guessed letter is in the word.
+    """
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    """
+    Prompt user for a letter and check if it's a valid guess.
+    """
+    while True:
+        guess = input("Guess a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            check_guess(guess)
+            break 
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+ask_for_input()
