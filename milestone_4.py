23516e2f1dbdacd6a8c468d50015020e54b70158
Jacob Softleigh-Moore <jacob.smoore@hotmail.co.uk>
2023-11-27 13:24:48 +0000

    added cli functionality to play whole game milestone 4

--- milestone_4.py
+++ milestone_4.py
@@ -11,11 +11,24 @@ class Hangman:
 
     def check_guess(self, guess):
         """
-        Check if the guessed letter is in the word.
+        Check if the guessed letter is in the word and update the game state accordingly.
         """
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+
+            # Check if the guess is a new correct guess
+            if guess not in self.word_guessed:
+                self.num_letters -= 1  # Decrease the number of unique letters left to guess
+
+            # Replace the underscores with the correctly guessed letter
+            for i, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[i] = guess
+        else:
+            # Handling incorrect guesses
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word. You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
         """
@@ -31,8 +44,18 @@ class Hangman:
             else:
                 self.list_of_guesses.append(guess)
                 self.check_guess(guess)
-                break
+                # Display the current state of the guessed word
+                print("Word guessed so far:", ' '.join(self.word_guessed))
+                if '_' not in self.word_guessed or self.num_lives <= 0:
+                    break  # Exit the loop if the game is over
 
+        # Check for win or lose
+        if '_' not in self.word_guessed:
+            print("Congratulations! You guessed the word:", self.word)
+        else:
+            print("Game over! The word was:", self.word)
+
+# Example usage
 favorite_fruits = ['apple', 'banana', 'cherry', 'plum', 'grape']
 game = Hangman(favorite_fruits)
 game.ask_for_input()
