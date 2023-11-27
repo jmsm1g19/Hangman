e13f44204c773f9c4ddbadb0dcbf269f8e6a5f98
Jacob Softleigh-Moore <jacob.smoore@hotmail.co.uk>
2023-11-27 14:25:58 +0000

    added comments to milestone 2

--- Checkpoints/milestone_2.py
+++ Checkpoints/milestone_2.py
@@ -1,15 +1,18 @@
 import random
 
+# List of words to choose from
 favorite_fruits = ['apple', 'banana', 'cherry', 'plum', 'grape']
 word_list = favorite_fruits
-word = random.choice(word_list)
+word = random.choice(word_list)  # Randomly selecting a word from the list
 
 print(word) 
 guess = input("Guess a letter: ")
 if len(guess) == 1 and guess.isalpha():
+    # Check if the guess is in the word
     if guess in word:
         print("Good guess!")
     else:
         print("Try again!")
 else:
+    # Handling invalid input
     print("Oops! That is not a valid input.")
